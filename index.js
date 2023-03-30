cfc3a05b7f78bd4811c3f0d0d941c00b4b512888
trevor wells <104236134+trevor-wells@users.noreply.github.com>
2023-03-29 19:47:51 -0600

    i think my Square isValid doesn't handle all cases but whatever

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// Your code here
+class Polygon{
+    constructor(sides){
+        this.sides = sides
+    }
+
+    get countSides(){
+        return this.sides.length
+    }
+
+    get perimeter(){
+        let sum = 0
+        for (const num of this.sides){
+            sum += num
+        }
+        return sum
+    }
+}
+
+class Triangle extends Polygon{
+    get isValid(){
+    return (this.countSides === 3
+            && this.sides[0] + this.sides[1] > this.sides[2]
+            && this.sides[0] + this.sides[2] > this.sides[1]
+            && this.sides[1] + this.sides[2] > this.sides[0])
+    }
+}
+
+class Square extends Polygon{
+    get area(){
+        return Math.pow(this.sides[0], 2)
+    }
+
+    get isValid(){
+        return (this.countSides === 4 && this.sides[0] === this.perimeter/4)
+    }
+}
